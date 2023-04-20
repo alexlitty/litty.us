d170afa326feeab535b599e93a2b65e233804c7e
Alexander Litty <alexander@litty.us>
2023-04-19 21:29:22 -0700

    Simplifying site to one-page experience.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import SiteHeader from './SiteHeader.jsx';
-import SiteSection from './SiteSection.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SiteHeader />
-      <SiteSection />
     </div>
   );
 }
