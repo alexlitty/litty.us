e7de583861e3bb32252b7ab41ac4c3103a8c0a64
Alexander Litty <alexander@litty.us>
2023-04-27 11:44:21 -0700

    Adding production serving helper script.

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+        name        : "serve_litty.us",
+        script : "bin/serve",
+        exec_interpreter : "none",
+        exec_mode   : 'fork',         
+        env: {
+            NODE_ENV: "production",
+        }
+    }]
+};
