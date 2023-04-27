979384820525019ac2579bd776e923b0dc512aca
Alexander Litty <alexander@litty.us>
2023-04-27 12:14:38 -0700

    Improving http and https serving deployment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,17 @@
 module.exports = {
     apps: [{
-        name: 'serve_litty.us',
-        script: 'bin/serve',
+        name: 'https_litty.us',
+        script: 'bin/serve-https',
         exec_interpreter: 'none',
         exec_mode: 'fork',         
+        env: {
+            NODE_ENV: 'production',
+        }
+    }, {
+        name: 'http_litty.us',
+        script: 'bin/serve-http',
+        exec_interpreter: 'none',
+        exec_mode: 'fork',
         env: {
             NODE_ENV: 'production',
         }
